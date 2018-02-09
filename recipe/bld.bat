13828319eb2ca78d64345aea39cce160cf3469f6
Min RK <benjaminrk@gmail.com>
2018-02-09 10:36:55 +0100

    i can has quotes right?

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 REM enable extensions via conf.d files for notebook >= 5.3
-set ETC=%CONDA_PREFIX%\etc\jupyter
-set SERVER_CONFIG=%ETC%\jupyter_notebook_config.d
-set TREE_CONFIG=%ETC%\nbconfig\tree.d
-if not exists %SERVER_CONFIG% mkdir %SERVER_CONFIG%
-if not exists %TREE_CONFIG% mkdir %TREE_CONFIG%
-copy %RECIPE_DIR%\serverextension.json %SERVER_CONFIG%/ipyparallel.json
-copy %RECIPE_DIR%\nbextension.json %TREE_CONFIG%/ipyparallel.json
+set "ETC=%CONDA_PREFIX%\etc\jupyter"
+set "SERVER_CONFIG=%ETC%\jupyter_notebook_config.d"
+set "TREE_CONFIG=%ETC%\nbconfig\tree.d"
+if not exists "%SERVER_CONFIG%" mkdir "%SERVER_CONFIG%"
+if not exists "%TREE_CONFIG%" mkdir "%TREE_CONFIG%"
+copy "%RECIPE_DIR%\serverextension.json" "%SERVER_CONFIG%/ipyparallel.json"
+copy "%RECIPE_DIR%\nbextension.json" "%TREE_CONFIG%/ipyparallel.json"
