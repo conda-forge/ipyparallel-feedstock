cc56361dc783079fdb86f94cd5b99c574cc8389e
Min RK <benjaminrk@gmail.com>
2018-02-12 11:01:06 +0100

    don't forget to install the package

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,5 +4,6 @@ set "SERVER_CONFIG=%ETC%\jupyter_notebook_config.d"
 set "TREE_CONFIG=%ETC%\nbconfig\tree.d"
 if not exist "%SERVER_CONFIG%" mkdir "%SERVER_CONFIG%"
 if not exist "%TREE_CONFIG%" mkdir "%TREE_CONFIG%"
-copy "%RECIPE_DIR%\serverextension.json" "%SERVER_CONFIG%/ipyparallel.json"
-copy "%RECIPE_DIR%\nbextension.json" "%TREE_CONFIG%/ipyparallel.json"
+copy "%RECIPE_DIR%\serverextension.json" "%SERVER_CONFIG%\ipyparallel.json"
+copy "%RECIPE_DIR%\nbextension.json" "%TREE_CONFIG%\ipyparallel.json"
+pip install --no-deps .
