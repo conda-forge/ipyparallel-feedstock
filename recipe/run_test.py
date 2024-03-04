59a3ef2f27d5116fc0537dd2b3c0a9aeb6d8cb2a
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-03-04 08:09:03 -0600

    update test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 import sys
-from subprocess import check_output, STDOUT
+from subprocess import check_output, call, STDOUT
 
 def sh(cmd):
-    out = check_output(cmd, stderr=STDOUT)
+    rc = call(cmd, encoding="utf-8")
+    if rc:
+        return ""
+    out = check_output(cmd, stderr=STDOUT, encoding="utf-8")
     return out.decode('utf8', 'replace')
 
-serverextensions = sh([sys.executable, '-m', 'jupyter', 'serverextension', 'list'])
+serverextensions = sh([sys.executable, '-m', 'jupyter', 'server', 'extension', 'list'])
 print(serverextensions)
 assert 'ipyparallel' in serverextensions
