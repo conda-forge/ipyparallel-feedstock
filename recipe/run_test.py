62c8c944f0f11ff186e58ca4d00606ffb21a1a48
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-03-04 08:17:23 -0600

    Update run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -7,9 +7,10 @@ def sh(cmd):
     rc = call(cmd, encoding="utf-8")
     if rc:
         return ""
-    out = check_output(cmd, stderr=STDOUT, encoding="utf-8")
-    return out.decode('utf8', 'replace')
+    out = check_output(cmd, stderr=STDOUT)
+    return out.decode("utf8", "replace")
 
-serverextensions = sh([sys.executable, '-m', 'jupyter', 'server', 'extension', 'list'])
-print(serverextensions)
-assert 'ipyparallel' in serverextensions
+if __name__ == "__main__":
+    serverextensions = sh([sys.executable, "-m", "jupyter", "server", "extension", "list"])
+    print(serverextensions)
+    assert "ipyparallel" in serverextensions
